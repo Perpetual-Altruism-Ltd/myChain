352e737a515981775d14af0a68f020fa650371ee
John Reed <87283488+jjreedv@users.noreply.github.com>
2022-04-05 12:16:06 -0700

    Finish Test Precompile

--- precompiles/prepay-gas/src/lib.rs
+++ precompiles/prepay-gas/src/lib.rs
@@ -1,33 +1,42 @@
 
-use evm_precompiles::{Gasometer, EvmDataWriter};
-use evm::{executor::stack::PrecompileOutput, Context, ExitSucceed, ExitError};
+use evm_precompiles::{EvmDataWriter};
+use utils::{Context, ExitError, ExitSucceed, Precompile, PrecompileOutput,
+	 PrecompileFailure};
 
+pub struct Blake2F;
 
-pub trait Precompile {
-	fn execute(
-		input: &[u8],
-		target_gas: Option<u64>,
-		context: &Context,
-		gasometer: &mut Gasometer,
-	) -> Result<PrecompileOutput, ExitError>;
+impl Blake2F {
+	const GAS_COST_PER_ROUND: u64 = 1; // https://eips.ethereum.org/EIPS/eip-152#gas-costs-and-benchmarks
 }
 
 pub struct TestPC1;
 
 impl Precompile for TestPC1 {
 	fn execute(
-        _input: &[u8], 
-        _target_gas: Option<u64>, 
+        input: &[u8], 
+        target_gas: Option<u64>, 
         _context: &Context,
-		gasometer: &mut Gasometer,
-    ) -> Result<PrecompileOutput, ExitError>{
+		_is_static: bool,
+    ) -> Result<PrecompileOutput, PrecompileFailure>{
+
+		let mut rounds_buf: [u8; 4] = [0; 4];
+		rounds_buf.copy_from_slice(&input[0..4]);
+		let rounds: u32 = u32::from_be_bytes(rounds_buf);
+
+		let gas_cost: u64 = (rounds as u64) * Blake2F::GAS_COST_PER_ROUND;
+
+		if let Some(gas_left) = target_gas {
+			if gas_left < gas_cost {
+				return Err(PrecompileFailure::Error{exit_status: ExitError::OutOfGas});
+			}
+		}
 
 		let message: &str = "Hello, World from MyChain!";
 		let result: Vec<u8> = message.as_bytes().to_vec();
 
 		Ok(PrecompileOutput {
 			exit_status: ExitSucceed::Returned,
-			cost: gasometer.used_gas(),
+			cost: gas_cost,
 			output: EvmDataWriter::new().write(result).build(),
 			logs: Default::default()
 		})
